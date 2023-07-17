d6c0e92a2355fa52aa54c03d311ab3f14110c9f9
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-07-17 19:05:38 +0500

    1 file modified, 1 added

--- src/components/Gallery/VirtualGallery/index.js
+++ src/components/Gallery/VirtualGallery/index.js
@@ -1,76 +1,18 @@
-import React, { useEffect, useRef } from 'react';
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { OrbitControls } from '@react-three/drei';
-import { Canvas, useLoader } from '@react-three/fiber';
-import { Stats } from '@react-three/drei';
+import Model from './3dModel';
 
-const VirtualGallery = () => {
-  const sceneRef = useRef();
-  const rendererRef = useRef();
-  const controlsRef = useRef();
-  const modelRef = useRef();
-  const modelPath = useLoader(GLTFLoader, './artgallery.glb');
-
-  useEffect(() => {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    camera.position.set(0, 0, 5);
-
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    rendererRef.current = renderer;
-
-    sceneRef.current.appendChild(renderer.domElement);
-
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controlsRef.current = controls;
-    controls.enableRotate = true;
-    controls.enableZoom = true;
-    controls.enablePan = true;
-
-    const loader = new GLTFLoader();
-    loader.load(modelPath, (gltf) => {
-      const model = gltf.scene;
-      modelRef.current = model;
-      scene.add(model);
-    });
-
-    const animate = () => {
-      requestAnimationFrame(animate);
-      renderer.render(scene, camera);
-      controls.update();
-    };
-
-    animate();
-
-    window.addEventListener('resize', () => {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-    });
-
-    return () => {
-      scene.remove(modelRef.current);
-      renderer.dispose();
-      sceneRef.current.removeChild(renderer.domElement);
-      controls.dispose();
-    };
-  }, []);
-
+export default function App() {
   return (
-    <Canvas>
-      <Stats />
-      <primitive object={modelRef.current} />
-      <OrbitControls />
-      <axesHelper args={[5]} />
-    </Canvas>
-  );
-};
+    <div className="App">
+      <Canvas camera={{ position: [0, 0, 10], fov: 55 }}>
+        <ambientLight intensity={0.6} />
+        <pointLight position={[10, 10, 10]} />
 
-export default VirtualGallery;
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
+
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
+}
