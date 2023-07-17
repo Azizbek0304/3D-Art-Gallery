6cd07dcd74ac48c67260f9a424316474da91109b
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-07-17 22:19:44 +0500

    1 file modified, 1 added, 1 deleted

--- src/components/Gallery/VirtualGallery/index.js
+++ src/components/Gallery/VirtualGallery/index.js
@@ -1,8 +1,24 @@
+import React, { useEffect, useRef } from 'react';
+import { Canvas } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-export default function VirtualGallery() {
-  const model = useLoader(GLTFLoader, './artgallery.glb');
-  console.log(model);
+const VirtualGallery = () => {
+  const sceneRef = useRef();
 
-  // ...
-}
+  useEffect(() => {
+    const loader = new GLTFLoader();
+    loader.load('./../../3dModel/small_art_gallery_space.glb', (gltf) => {
+      sceneRef.current.add(gltf.scene);
+    });
+  }, []);
+
+  return (
+    <div className="canvas-container">
+      <Canvas>
+        <scene ref={sceneRef} />
+      </Canvas>
+    </div>
+  );
+};
+
+export default VirtualGallery;
